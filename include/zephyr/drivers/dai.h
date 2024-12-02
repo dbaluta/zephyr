e4047cd0d2e6015fe1955baae87b358ed9f59fc6
Daniel Baluta <daniel.baluta@nxp.com>
2024-12-02 15:49:49 +0200

    modify dai index
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- include/zephyr/drivers/dai.h
+++ include/zephyr/drivers/dai.h
@@ -114,8 +114,8 @@ enum dai_type {
 	DAI_INTEL_SSP_NHLT,	/**< nhlt ssp */
 	DAI_INTEL_DMIC_NHLT,	/**< nhlt ssp */
 	DAI_INTEL_HDA_NHLT,	/**< nhlt Intel HD/A */
-	DAI_INTEL_ALH_NHLT,	/**< nhlt Intel ALH */
 	DAI_IMX_MICFIL,		/**< i.MX PDM MICFIL */
+	DAI_INTEL_ALH_NHLT,	/**< nhlt Intel ALH */
 };
 
 /**
